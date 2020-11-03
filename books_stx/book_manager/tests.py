f6432a05dd2b993d97adb583a3b68855a30f8f95
bartek <bartosz.kobylinski@gmail.com>
2020-11-03 17:18:22 +0100

    ADDING MODEL TO DATABASE
    - adding model to database
    - adding test for retrieving and saving book model

--- books_stx/book_manager/tests.py
+++ books_stx/book_manager/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from book_manager.models import Book
 
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -14,3 +15,19 @@ class TestDjangoInstallation(TestCase):
         driver.get("http://127.0.0.1:8000")
         browser = driver
         self.assertIn("Django", browser.title)
+
+
+class TestBookMode(TestCase):
+
+    def test_retrieving_and_saving_book_model(self):
+        book = Book(
+            author='Author',
+            title="Title",
+            publish_year=1520,
+            isbn=145841451,
+            pages=152,
+            cover='http:/eueu',
+            language="pl")
+        book.save()
+        books_query = Book.objects.all()
+        self.assertEqual(books_query.count(), 1)
