b04de8a1632470975696f25f52509f42447c5921
bartek <bartosz.kobylinski@gmail.com>
2020-11-06 14:46:10 +0100

    ADDING FUNCTIONALTY OF IMPORTING BOOKS FROM GOOGLE API
    - adding template for form importing books rendering
    - modifying bookModel attribute to null=True to mark in database that it can have a null value
    - adding tests
    - modyfing some minor errors

--- books_stx/book_manager/models.py
+++ books_stx/book_manager/models.py
@@ -3,13 +3,17 @@ from django.urls import reverse
 
 
 class Book(models.Model):
-    author = models.CharField(max_length=50)
-    title = models.CharField(max_length=250)
-    publish_year = models.DecimalField(max_digits=4, decimal_places=0)
-    isbn = models.CharField(max_length=13)
-    pages = models.PositiveIntegerField()
-    cover = models.URLField()
-    language = models.CharField(max_length=2)
+    authors = models.CharField(max_length=100, null=True)
+    title = models.CharField(max_length=250, null=True)
+    publish_year = models.DecimalField(max_digits=4, decimal_places=0, null=True)
+    isbn_10 = models.CharField(max_length=10, blank=True, null=True)
+    isbn_13 = models.CharField(max_length=13, blank=True, null=True)
+    oclc_number = models.CharField(max_length=25, blank=True, null=True)
+    lccn_number = models.CharField(max_length=25, blank=True, null=True)
+    pages = models.PositiveIntegerField(null=True)
+    cover = models.URLField(blank=True)
+    language = models.CharField(max_length=2, null=True)
+    subject = models.CharField(max_length=50, blank=True, null=True)
 
     def get_absolute_url(self):
         return reverse('books')
--- books_stx/book_manager/urls.py
+++ books_stx/book_manager/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from book_manager.views import BookListView, AddBookView
+from book_manager.views import BookListView, AddBookView, GoogleApiView
 
 urlpatterns = [
     path("books/", BookListView.as_view(), name="books"),
     path("add_book/", AddBookView.as_view(), name="add_book"),
+    path("import_books", GoogleApiView.as_view(), name='import_books'),
 ]
