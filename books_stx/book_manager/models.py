1dd4f8b9d0500df5f56121fe02b4c48bd6ff7287
bartek <bartosz.kobylinski@gmail.com>
2020-11-07 22:04:58 +0100

    MODIFYING CHANGES WHEN GOOGLE API IMPORT
    - adding function to change color and text when user click on upload button to inform user that action has been taken and process is working
    - some changes in long fields of authors

--- books_stx/book_manager/models.py
+++ books_stx/book_manager/models.py
@@ -4,7 +4,7 @@ from django.core.validators import URLValidator
 
 
 class Book(models.Model):
-    authors = models.CharField(max_length=100, null=True)
+    authors = models.CharField(max_length=250, null=True)
     title = models.CharField(max_length=250, null=True)
     publish_year = models.DecimalField(max_digits=4, decimal_places=0, null=True)
     isbn_10 = models.CharField(max_length=10, blank=True, null=True)
